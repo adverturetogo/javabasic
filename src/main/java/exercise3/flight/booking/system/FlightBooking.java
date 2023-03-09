27c1d0e802a68acaf5dce818bebe7cdb8a7df1e3
adverturetogo <2323100321@qq.com>
2023-03-09 17:45:29 +0800

    12

--- src/main/java/exercise3/flight/booking/system/FlightBooking.java
+++ src/main/java/exercise3/flight/booking/system/FlightBooking.java
@@ -1,5 +1,80 @@
 package exercise3.flight.booking.system;
 
+import java.time.LocalDate;
+
 public class FlightBooking {
+   
+
+   private String PassengerFullName;
+   private String TicketNumber;
+   private String FlightCompany;
+   private LocalDate DepartureDate;
+   private LocalDate ReturnDate;
+   private String TripSource;
+   private String TripDestination;
+   private int TotalPassengers;
+   private int TotalTicketPrice;
+   private int ChildPassengers;
+   private int AdultPassengers;
+   public FlightBooking(String PassengerFullName,LocalDate DepartureDate,LocalDate ReturnDate,int TotalPassengers,int TotalTicketPrice ){
+
+   }
+    public String getPassengerFullName()
+    {
+        return PassengerFullName;
+    }
+    public String getTicketNumber()
+        {
+            return TicketNumber;
+        }
+    public String getFlightCompany()
+    {
+        return FlightCompany;
+    }
+    public LocalDate getDepartureDate()
+    {
+        return DepartureDate;
+    }
+    public LocalDate getReturnDate()
+    {
+        return ReturnDate;
+    }
+    public String getTripSource()
+    {
+        return TripSource;
+    }
+    public String getTripDestination()
+    {
+        return TripDestination;
+    }
+
+    public int setTotalPassengers(int ChildPassengers,int AdultPassengers){
+       int  TotalPassengers=ChildPassengers+AdultPassengers;
+       return TotalPassengers;
+    }
+
+
+    public int getTotalPassengers()
+    {
+        return TotalPassengers;
+    }
+    public int getTotalTicketPrice()
+    {
+        return TotalTicketPrice;
+    }
+    public int getChildPassengers()
+    {
+        return ChildPassengers;
+    }
+    public int getAdultPassengers()
+    {
+        return AdultPassengers;
+    }
     
+   
+    
+
+
+
+
 }
--- src/main/java/exercise3/flight/booking/system/Main.java
+++ src/main/java/exercise3/flight/booking/system/Main.java
@@ -1,10 +1,62 @@
 package exercise3.flight.booking.system;
+import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
 
 
 public class Main 
 {
+    public static Date StrToDate(String  str)
+    {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try{
+            date = format.parse(str);
+        }
+        catch(ParseException e)
+        {
+            e.printStackTrace();
+        }    
+        return date;
+     }
     public static void main( String[] args )
     {
+        String flightCompany =" Flights-of-Fancy";
+        String tripSource ="NANJING";
+        String destinationAirport="OULU AIRPORT";
+        Scanner in = new Scanner (System.in);
+        System.out.println("Your name");
+        String passengerFullName = in.nextLine();
+        System.out.println("The departureDate in the form of yyyy-MM-dd");
+        String departureDate= in.nextLine();
+        System.out.println("The returnDate in the form of yyyy-MM-dd");
+        String returnDate = in.nextLine();
+        int childPassengers = 0;
+        int adultPassengers = 0;
+        System.out.println("The number of childPassengers is");
+         childPassengers = in.nextInt();
+         System.out.println("The number of adult is");
+         adultPassengers = in.nextInt();
+         int totalPassengers =childPassengers+adultPassengers;
         
+        int totalTicketPrice = childPassengers*200+adultPassengers*300;
+        Random r = new Random();
+        int ticketNumber =r.nextInt(100000000);
+        
+        System.out.println( "Dear " + passengerFullName + ". Thank you for booking your flight with " + 
+        flightCompany + ". Following are the details of your booking and the trip:" + "\n" + 
+        "Ticket Number: " + ticketNumber + "\n" + 
+        "From " + tripSource + " to " + destinationAirport + "\n" +
+        "Date of departure: " +StrToDate(departureDate)  + "\n" +
+        "Date of return: " + StrToDate(returnDate) + "\n" +
+        "Total passengers: " + totalPassengers + "\n" +
+        "Total ticket price in Euros: " + totalTicketPrice);
+        in.close();
     }
-}
+       
+            
+    }
+
+    
